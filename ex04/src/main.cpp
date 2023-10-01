13470c3b0ec562f7f390d530d21425245526419c
Lisa <lduheron@e3r8p4.42.fr>
2023-10-01 17:31:29 +0200

    Functional ex04 (sill need to protect from errors)

--- ex04/src/main.cpp
+++ ex04/src/main.cpp
@@ -6,14 +6,34 @@
 /*   By: lduheron <lduheron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 16:15:49 by lduheron          #+#    #+#             */
-/*   Updated: 2023/09/24 15:09:23 by lduheron         ###   ########.fr       */
+/*   Updated: 2023/10/01 17:28:10 by lduheron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <cstring>
 #include <fstream>
 #include <iostream>
 #include <string>
-#include <algorithm>
+
+std::string sed(std::string line, std::string OldWord, std::string NewWord)
+{
+	size_t			i = 0;
+	size_t			pos = 0;
+	int				len_Old_Word = OldWord.length();
+
+	while (i < line.length())
+	{
+		pos = line.find(OldWord, i);
+		if (pos != std::string::npos)
+		{
+			line.erase(pos, len_Old_Word);
+			line.insert(pos, NewWord);
+			i += pos + len_Old_Word;
+		}
+		i++;
+	}
+	return (line);
+}
 
 int	main(int argc, char **argv)
 {
@@ -22,59 +42,18 @@ int	main(int argc, char **argv)
 		std::cout << "Please enter 3 arguments.\n";
 		return (0);
 	}
-	std::string		filename = argv[1];
-	std::string		outfile_name = filename + ".replace";
-	std::string		s1 = argv[2];
-	std::string		s2 = argv[3];
 	std::ifstream	infile(argv[1]);
-	std::ofstream 	outfile(outfile_name.c_str());
-
-	if (s1.empty() || s2.empty())
-		return (0);
-
-	// ouvrir infile
-	// infile.open(argv[1]);
-		// return (ERROR);
-
-	// creer le fichier
-	outfile.is_open();
-	// boucler pour remplacer
-	// outfile << "test";
-
+	std::string		filename = argv[1];
+	std::ofstream	outfile(filename.append(".replace").c_str());
 	std::string		line;
-	unsigned long		len = s1.length();
-		int					pos = 0;
-		unsigned long		cpt = 0;
-	std::getline(infile, line);
-	while (!line.empty())
+
+	outfile.is_open();
+	while (std::getline(infile, line))
 	{
-		while (line[pos])
-		{
-			if (!line[pos])
-				break;
-			cpt = 0;
-			if (line[pos] == s1[cpt])
-			{
-				std::cout << "line [pos] : " << line[pos] << " s1[cpt] : " << s2[cpt] << "\n";
-				while (line[pos + cpt] == s1[cpt])
-					cpt++;
-				std::cout << "cpt : " << cpt << " len : " << len << "\n";
-				if (cpt == len)
-				{
-					std::cout << "EQUIVALENT\n";
-					outfile << s2;
-					pos+= cpt;
-				}
-			}
-			outfile << line[pos];
-			pos++;
-		}
-		outfile <<  '\n';
-		std::getline(infile, line);
+		line = sed(line, argv[2], argv[3]);
+		outfile << line << "\n";
 	}
 	infile.close();
 	outfile.close();
 	return (0);
 }
-
-// https://cplusplus.com/reference/sstream/stringstream/
