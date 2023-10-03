5e45961338d0f0680a8e6a0926df542394815f7f
Lisa <lduheron@e3r8p4.42.fr>
2023-10-03 11:13:42 +0200

    fix ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,28 +6,11 @@
 /*   By: lduheron <lduheron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 14:44:59 by lduheron          #+#    #+#             */
-/*   Updated: 2023/10/01 17:47:15 by lduheron         ###   ########.fr       */
+/*   Updated: 2023/10/03 11:09:46 by lduheron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-/*
-
-* pointeur 
-& adresse
-
-Une reference est un alias vers une variable existante. 
-Une reference est un pointeur constant qui est toujours deference et jamais nul.
-Dereferencer = mettre une etoile devant ppour dire quon est interesse par ce qui est pointe et non pas le pointeur.
-
-Une reference comme un pointeur stocke l'adresse d'un objet situe ailleurs dans la memoire.
-ontrairement à un pointeur, une référence après son initialisation ne peut pas être définie
-pour faire référence à un autre objet ni prendre la valeur null.
-
-https://waytolearnx.com/2018/09/difference-entre-pointeur-et-reference.html
-
-*/
 
 int	main(void)
 {
