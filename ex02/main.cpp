81015d310c174632ba22274d2d1ed335e4101cdf
Lisa <lduheron@e3r8p4.42.fr>
2023-10-03 11:23:14 +0200

    norme

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lduheron <lduheron@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/15 14:44:59 by lduheron          #+#    #+#             */
-/*   Updated: 2023/10/03 11:09:46 by lduheron         ###   ########.fr       */
+/*   Updated: 2023/10/03 11:21:53 by lduheron         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,8 @@ int	main(void)
 	std::cout << &stringContent << " is the address of stringContent" << std::endl;
 	std::cout << stringPTR << " is the address of stringPTR" << std::endl;
 	std::cout << &stringREF << " is the address of stringREF\n" << std::endl;
-
 	std::cout << stringContent << " is the value of stringContent" << std::endl;
 	std::cout << *stringPTR << " is the value of stringPTR" << std::endl;
 	std::cout << stringREF << " is the value of stringREF" << std::endl;
-
 	return (0);
 }
